995061147a912670a7a5fb3c5a965e336c82693c
Yaroslaw-aw <manumission@mail.ru>
2023-06-08 20:09:48 +0300

    Task 3 complete

--- Program.cs
+++ Program.cs
@@ -4,25 +4,54 @@
     {
         static void Main(string[] args)
         {
-            // Задача 1) Вычислить n-ое треугольного число(сумма чисел от 1 до n), n!(произведение чисел от 1 до n)
+            // Задача 3) Реализовать простой калькулятор
+            // + - / *
+            //Введите первое число: A \n
+            //Введите знак: *
+            //Введите второе число: B
+            //Результат: A* B
 
-            int number = Input();
+            double number1 = InputNumber("Введите первое число: ");
+            string sign = InputSignOfOperation("Введите знак операции: + или - или * или /");
+            double number2 = InputNumber("Введите второе число: ");
 
-            Summa(number);
-            Factorial(number);
+            switch (sign)
+            {
+                case "+":
+                    {
+                        Addition(number1, number2);
+                        break;
+                    }
+                case "-":
+                    {
+                        Subtraction(number1, number2);
+                        break;
+                    }
+                case "*":
+                    {
+                        Multiplication(number1, number2);
+                        break;
+                    }
+                case "/":
+                    {
+                        Division(number1, number2);
+                        break;
+                    }
+            }
+
 
 
             ///<summary>
             ///Ввод числа
             ///</summary>
-            int Input(string message = "Введите число: ")
+            double InputNumber(string message = "Введите число: ")
             {
                 Console.WriteLine(message);
-                bool isNumbercorrect = int.TryParse(Console.ReadLine(), out int number);
+                bool isNumbercorrect = double.TryParse(Console.ReadLine(), out double number);
 
                 if (!isNumbercorrect || number < 1)
                 {
-                    return Input("Вы ввели недопустимое значение, попробуйте ещё раз");
+                    return InputNumber("Вы ввели недопустимое значение, попробуйте ещё раз");
                 }
                 else
                 {
@@ -31,24 +60,54 @@
             }
 
             ///<summary>
-            ///Расчет суммы чисел от 1 до заданного по формуле Гауса
-            /// </summary>
-            void Summa(int number)
+            ///Ввод знака операции
+            ///</summary>
+            string InputSignOfOperation(string message = "Введите знак операции: + или - или * или /")
             {
-                Console.WriteLine($"Сумма чисел от 1 до {number} равна: {(number * (number + 1)/2)}");
+                Console.WriteLine(message);
+                string sign = Console.ReadLine();
+
+                if (sign == "+" || sign == "-" || sign == "*" || sign == "/")
+                {
+                    return sign;
+                }
+                else
+                {
+                    return InputSignOfOperation("Вы ввели неверный символ, попробуйте ещё раз (+ или - или * или /): ");
+                }
+
             }
 
             ///<summary>
-            ///Расчет факториала числа
+            ///Сумма чисел
             /// </summary>
-            void Factorial(int number)
+            void Addition(double number1, double number2)
             {
-                int fact = 1;
-                for (int i = 1; i <= number; i++)
-                {
-                    fact *= i;
-                }
-                Console.WriteLine($"Факториал числа равен: {fact}");
+                Console.WriteLine($"Сумма чисел {number1} + {number2} = {number1 + number2}");
+            }
+
+            ///<summary>
+            ///Разность чисел
+            /// </summary>
+            void Subtraction(double number1, double number2)
+            {
+                Console.WriteLine($"Разность чисел {number1} - {number2} = {number1 - number2}");
+            }
+
+            ///<summary>
+            ///Умножение чисел
+            /// </summary>
+            void Multiplication(double number1, double number2)
+            {
+                Console.WriteLine($"Произведение чисел {number1} * {number2} = {number1 * number2}");
+            }
+
+            ///<summary>
+            ///Деление чисел
+            /// </summary>
+            void Division(double number1, double number2)
+            {
+                Console.WriteLine($"Частное от деления числа {number1} на число {number2} = {number1 / number2}");
             }
         }
     }
